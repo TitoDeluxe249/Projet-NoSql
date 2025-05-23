f5fa3c0ad332a79d3f1edd55c1f68d16f09891f4
Terence Amba <terence.amba@citepa.org>
2025-05-23 15:10:50 +0200

    THE THIRD

--- mon_script.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["Catalogue_de_produits"]
-
-produit = {"nom": "iPhone 14", "prix": 1099, "stock": 10}
-db.produits.insert_one(produit)
-
-print("Produit ajouté.")
